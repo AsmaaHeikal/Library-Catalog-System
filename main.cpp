1111c2e231f671f5f62a889fc5f44d169b0083f1
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-11-30 16:02:50 +0200

    update files

--- main.cpp
+++ main.cpp
@@ -11,14 +11,6 @@ struct Book {
     char bookTitle[30];
     char authorID[15];
 };
-struct PrimaryIndex {
-    char key[15];
-    int RNN;
-};
-struct SecondaryIndex {
-    char attribute[15];
-    int primaryKey;
-};
 
 int main() {
     while (true) {
@@ -40,28 +32,41 @@ int main() {
 
         switch (choice) {
             case 1: {
-                // Add New Author
-                // Get author details from the user and call addAuthor function
-                Author author;
-                cout<< "enter author name: ";
-                cin>>author.authorName;
-                cout<< "enter author address: ";
-                cin>>author.address;
                 break;
             }
             case 2: {
-                // Add New Book
-                // Get book details from the user and call addBook function
+
                 break;
             }
             case 3: {
-                // Update Author Name
-                // Get author ID and new name from the user and call updateAuthorName function
+
                 break;
             }
-                // Add cases for other options
+            case 4: {
+
+                break;
+            }
+            case 5: {
+
+                break;
+            }
+            case 6: {
+
+                break;
+            }
+            case 7: {
+
+                break;
+            }
+            case 8: {
+
+                break;
+            }
+            case 9: {
+
+                break;
+            }
             case 10: {
-                // Exit the program
                 return 0;
             }
             default: {
